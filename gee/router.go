0b85b815af685e4a4428c42d8281ccdefb4b7ba4
ss qaq <2112536022@qq.com>
2023-05-29 22:31:39 +0800

    feat: 增则增加前路由ç¼   前缀树

--- gee/router.go
+++ gee/router.go
@@ -1,24 +1,80 @@
 package gee
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 type Router struct {
+	roots    map[string]*node
 	handlers map[string]HandleFunc
 }
 
 func newRouter() *Router {
-	return &Router{handlers: make(map[string]HandleFunc)}
+	return &Router{
+		roots:    make(map[string]*node),
+		handlers: make(map[string]HandleFunc),
+	}
 }
 
+func parsePattern(pattern string) []string {
+	vs := strings.Split(pattern, "/")
+
+	parts := make([]string, 0)
+
+	for _, part := range vs {
+		if part != "" {
+			parts = append(parts, part)
+			if part[0] == '*' {
+				break
+			}
+		}
+	}
+
+	return parts
+}
 func (r *Router) addRouter(method string, pattern string, handle HandleFunc) {
+	parts := parsePattern(pattern)
 	key := method + "-" + pattern
+	_, ok := r.roots[method]
+	if !ok {
+		r.roots[method] = &node{}
+	}
+	r.roots[method].insert(pattern, parts, 0)
 	r.handlers[key] = handle
 }
 
+func (r *Router) getRoute(method string, pattern string) (*node, map[string]string) {
+	searchParts := parsePattern(pattern)
+	params := make(map[string]string)
+
+	root, ok := r.roots[method]
+	if !ok {
+		return nil, nil
+	}
+	res := root.search(searchParts, 0)
+	if res != nil {
+		parts := parsePattern(res.pattern)
+		for idx, part := range parts {
+			if part[0] == ':' {
+				params[part[1:]] = searchParts[idx]
+			}
+			if part[0] == '*' && len(part) > 1 {
+				params[part[1:]] = strings.Join(searchParts[idx:], "/")
+				break
+			}
+		}
+		return res, params
+	}
+	return nil, nil
+
+}
 func (r *Router) handle(c *Context) {
-	key := c.Method + "-" + c.Path
-	if handler, ok := r.handlers[key]; ok {
-		handler(c)
+	n, params := r.getRoute(c.Method, c.Path)
+	if n != nil {
+		c.Params = params
+		key := c.Method + "-" + n.pattern
+		r.handlers[key](c)
 	} else {
 		c.String(http.StatusNotFound, "404 NOT FOUND %s\n", c.Path)
 	}
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"gee"
+	"net/http"
 )
 
 func main() {
@@ -9,7 +10,10 @@ func main() {
 	e.GET("/", func(c *gee.Context) {
 		c.HTML(200, "<h1>杨恩文大傻逼</h1>")
 	})
+	e.GET("/hello/:name", func(c *gee.Context) {
 
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+	})
 	e.Run(":9999")
 
 }
